2bdce8133bfbd2bcb8ba86cd2416b01543c16c47
adam <git@adamrgrey.com>
2026-03-14 10:01:11 -0400

    this isn't a good place either i think

--- bash/jenkinslint.sh
+++ bash/jenkinslint.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-token=$(cat .local/bin/jenkinstoken.txt)
+token=$(cat ~/.local/bin/jenkinstoken.txt)
 
 curl -X POST --user "$token" -F "jenkinsfile=<./Jenkinsfile" alloces.lan:8080/pipeline-model-converter/validate
